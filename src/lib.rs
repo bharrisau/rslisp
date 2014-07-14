673c9451a7a174b52c96761e484562e762e2aff3
Ben Harris <mail@bharr.is>
2014-07-14 16:40:40 +0800

    Starting the work

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![crate_name = "rlisp"]
+#![deny(missing_doc)]
+#![warn(dead_code)]
+
+//! A Scheme R7RS implementation in Rust
+
+// Extern crates
+
+// Re-exports
+
+// Public modules
+
+// Private modules
+mod parse;
---
+++ src/parse/mod.rs
@@ -0,0 +1,4 @@
+
+#[test]
+fn dummy_test() {
+}
